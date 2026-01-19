11284d11f0f271bb67a0f774209891b2720b4061
Nic <carlson1858@gmail.com>
2026-01-19 15:07:57 -0600

    Fix json issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBe2FU3zcKzQPcEAe02PAGO7yna9QXjzMk",
-    authDomain: "inventory-setups.firebaseapp.com",
-    projectId: "inventory-setups",
-    storageBucket: "inventory-setups.appspot.com",
-    messagingSenderId: "213714247131",
-    appId: "1:213714247131:web:52b165043e878d1315dbfe",
-    measurementId: "G-PNKB6J8T48"
+    apiKey: "AIzaSyD4wwv2RkhpjaRfesqLWR4pe7eW84n3lVQ",
+    authDomain: "osrs-setups-nc.firebaseapp.com",
+    projectId: "osrs-setups",
+    storageBucket: "osrs-setups.firebasestorage.app",
+    messagingSenderId: "629115383666",
+    appId: "1:629115383666:web:1219823e2b1b50912df7eb",
+    measurementId: "G-8EEG05C8LJ"	
   },
   recaptcha: {
     siteKey: '6LdCfJwqAAAAAPYzwte1Ie0A-hGzRT4od1ZXJ-qe'
